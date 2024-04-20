deee8b9c6a2a243e8a7f7843baa732262446203b
ChandrakumarV <chandrakumar252004@gmail.com>
2024-04-20 10:19:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  https.get("https://jsonplaceholder.typicode.com/todos/39", (response) => {
+    console.log(response.statusCode);
+
+    response.on("data", (data) => {
+      const todos = JSON.parse(data);
+      console.log(todos);
+      res.status(200).json(todos);
+    });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
